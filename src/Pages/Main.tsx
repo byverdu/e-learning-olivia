4b3382715cd514431bc4642b308909278148e520
Albert Vallverdu <Albert.Vallverdu@reachplc.com>
2020-08-06 02:04:35 +0100

    typscript/react/scss/dev setup

---
+++ src/Pages/Main.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./main.scss";
+
+const Main: React.FunctionComponent = () => (
+  <>
+    <h1 className={styles.title}>xoxo 2</h1>
+    <p className={styles["xoxo-2"]}>live is ok</p>
+  </>
+);
+
+export default Main;
---
+++ webpack.config.js
@@ -0,0 +1,105 @@
+const path = require("path");
+const webpack = require("webpack");
+const { TsconfigPathsPlugin } = require("tsconfig-paths-webpack-plugin");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const ENV = process.env.ENV;
+
+module.exports = function () {
+  const outputPath =
+    ENV === "development" ? __dirname + "/dev-build" : __dirname + "/build";
+
+  return {
+    mode: ENV,
+    entry: "./src/index.tsx",
+
+    cache: false,
+
+    output: {
+      path: outputPath,
+      filename: "script/bundle.js",
+      libraryTarget: "umd",
+    },
+
+    devServer: {
+      contentBase: path.join(__dirname, outputPath),
+      compress: true,
+      port: 9000,
+      hot: true,
+    },
+
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: ENV === "development" ? "source-map" : undefined,
+
+    resolve: {
+      // Add '.ts' and '.tsx' as resolvable extensions.
+      extensions: [".ts", ".tsx", ".js", ".scss", ".json"],
+      modules: [path.join(__dirname, "node_modules")],
+      plugins: [
+        new TsconfigPathsPlugin({
+          configFile: "./tsconfig.json",
+          baseUrl: "src",
+        }),
+      ],
+    },
+
+    plugins: [
+      new webpack.WatchIgnorePlugin([/css\.d\.ts$/]),
+      new CleanWebpackPlugin({
+        cleanStaleWebpackAssets: false,
+      }),
+
+      // Webpack plugin that runs TypeScript type checker on a separate process.
+      new ForkTsCheckerWebpackPlugin(),
+      new HtmlWebpackPlugin({ template: "./src/index.html" }),
+
+      // new CopyPlugin([
+      //   {
+      //     from: "src/images/*",
+      //     to: "./images",
+      //     flatten: true,
+      //   },
+      //   {
+      //     from: "manifest.json",
+      //     to: "./",
+      //   },
+      // ]),
+    ],
+
+    module: {
+      rules: [
+        {
+          test: /\.ts(x?)$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: "ts-loader",
+              options: {
+                transpileOnly: true,
+              },
+            },
+          ],
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            "style-loader",
+            "css-modules-typescript-loader",
+            {
+              loader: "css-loader",
+              options: {
+                modules: {
+                  localIdentName: "[name]__[local]--[hash:base64:5]",
+                },
+              },
+            },
+            "sass-loader",
+          ],
+        },
+      ],
+    },
+  };
+};
