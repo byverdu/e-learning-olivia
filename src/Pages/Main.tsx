30f8c43c5ac21fde759fc7f39a819b51bb1d477d
Albert Vallverdu <Albert.Vallverdu@reachplc.com>
2020-08-07 09:51:45 +0100

    added eslint config

--- src/Pages/Main.tsx
+++ src/Pages/Main.tsx
@@ -3,9 +3,13 @@ import styles from './main.scss'
 
 const Main: React.FunctionComponent = () => (
   <>
-    <h1 className={styles.title}>xoxo 2</h1>
-    <p id="xoxo" className={styles['xoxo-2']}>
-      live is ok
+    <h1 className={styles.title}>Hello World</h1>
+    <p
+      id="xoxo"
+      className={styles.xoxo}
+      draggable
+    >
+      Live is ok
     </p>
   </>
 )
