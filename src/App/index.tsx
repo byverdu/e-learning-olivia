1d23c79237326f31c7ce6539ebf75a46112fa034
Albert Vallverdu <byverdu@gmail.com>
2020-08-17 01:15:28 +0100

    renamed Pages component to PageContainer

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Scoreboard from 'Components/Molecules/Scoreboard'
 import CountSelector from 'Components/Molecules/CountSelector'
+import PageContainer from 'Components/Molecules/PageContainer'
 import { Store } from 'Store'
 
 const App: React.FunctionComponent = () => (
   <Store>
-    <CountSelector />
-    <Scoreboard />
+    <PageContainer activePage="main">
+      <CountSelector />
+      <Scoreboard />
+    </PageContainer>
   </Store>
 )
 
