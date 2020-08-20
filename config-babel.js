aef67ea339048a826d69215eded06a6670278ac2
Albert Vallverdu <byverdu@gmail.com>
2020-08-20 21:15:12 +0100

    add plugin-proposal-class-properties to babel

--- config-babel.js
+++ config-babel.js
@@ -18,6 +18,7 @@ module.exports = () => ({
   ],
   plugins: [
     '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-proposal-class-properties',
     [
       '@babel/plugin-transform-runtime',
       {
