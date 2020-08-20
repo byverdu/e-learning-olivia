3009c7cd1e9f9cedf71e82461a36b90625fff6f5
Albert Vallverdu <byverdu@gmail.com>
2020-08-20 11:13:14 +0100

    getYouTubeSearchResults to filter none live videos and return videoId[]

--- src/utils/getYouTubeSearchResults.ts
+++ src/utils/getYouTubeSearchResults.ts
@@ -1,7 +1,50 @@
-import { from } from 'rxjs'
-import { map } from 'rxjs/operators'
+import { from, of } from 'rxjs'
+import { fromFetch } from 'rxjs/fetch'
+import { map, switchMap, catchError, tap } from 'rxjs/operators'
+
+interface YoutubeItem {
+  id: {
+    videoId: string
+  }
+  snippet: {
+    liveBroadcastContent: 'live' | 'none'
+  }
+}
+
+interface YoutubeResp {
+  items: YoutubeItem[]
+}
+
+const fetchData = async (params: string) =>
+  fromFetch(
+    `http://localhost:9000/youtube-search?search=${params}`,
+  ).pipe(
+    switchMap(response => {
+      if (response.ok) {
+        // OK return data
+        return response.json()
+      }
+      // Server is returning a status requiring the client to try something else.
+      return of({ error: true, message: `Error ${response.status}` })
+    }),
+    catchError(err => {
+      // Network or other error, handle appropriately
+      console.error(err)
+      return of({ error: true, message: err.message })
+    }),
+  )
 
 export const getYouTubeSearchResults = async (params: string) =>
-  from(fetch(`http://localhost:9000/youtube-search?search=${params}`))
-    .pipe(map(apiResp => apiResp.json()))
-    .toPromise()
+  from(
+    (await fetchData(params)).pipe(
+      map((apiResp: YoutubeResp) =>
+        apiResp.items
+          .filter(
+            ({ snippet: { liveBroadcastContent } }) =>
+              liveBroadcastContent === 'none',
+          )
+          .map(({ id: { videoId } }) => videoId),
+      ),
+      tap(console.log),
+    ),
+  ).toPromise()
