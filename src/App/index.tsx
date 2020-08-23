410397c5dce7e791bfeae2e90a4ccb9c47fc31b4
Albert Vallverdu <byverdu@gmail.com>
2020-08-23 01:35:45 +0100

    refactored Components tree for main app

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,22 +1,13 @@
 import React from 'react'
 import Store from 'Store'
 
-import Scoreboard from 'Components/Molecules/Scoreboard'
-import CountSelector from 'Components/Molecules/CountSelector'
-import PageContainer from 'Components/Molecules/PageContainer'
-import YouTubeVideo from 'Components/Atoms/YouTubeVideo'
-import YouTubeSearch from 'Components/Atoms/YouTubeSearch'
+import PageContainer from 'Components/Pages/Container'
 
 import './index.scss'
 
 const App: React.FunctionComponent = () => (
   <Store>
-    <PageContainer activePage="main">
-      <YouTubeSearch />
-      <CountSelector />
-      <Scoreboard />
-      <YouTubeVideo />
-    </PageContainer>
+    <PageContainer />
   </Store>
 )
 
---
+++ src/Components/Pages/Container/PageContainer.tsx
@@ -0,0 +1,57 @@
+import React, { FunctionComponent, useContext } from 'react'
+
+import { AppContext } from 'Store'
+import { PagesType } from 'Store/store.types';
+
+import Loader from 'Components/Atoms/Loader';
+import PageSearch from 'Components/Pages/Search';
+import ErrorPage from 'Components/Pages/Error';
+
+import styles from './pageContainer.scss'
+
+const pageTitles = new Map<PagesType, string>([
+  ['main', 'Select Your Game Type'],
+  ['search', 'Search Videos on Youtube'],
+])
+
+const Temp = () => <div>Temp</div>
+
+const PageComponent:FunctionComponent<{page: PagesType}> = ({ page }) => {
+  const components: {[key in PagesType]: FunctionComponent} = {
+    search: PageSearch,
+    error: ErrorPage,
+    main: Temp,
+  }
+  const ToRender = components[page]
+
+  return (
+    <ToRender />
+  )
+}
+
+const PageContainer: FunctionComponent = ({
+  children,
+}) => {
+  const { state: { loader, activePage } } = useContext(AppContext)
+  const title = pageTitles.get(activePage)
+
+  return (
+    <>
+      {loader.active ? (
+        <Loader
+          size="large"
+          text={loader.text}
+        />
+      )
+        : (
+          <section className={styles.container}>
+            <h1 className={styles.title}>{title}</h1>
+            <PageComponent page={activePage} />
+            {children}
+          </section>
+        )}
+    </>
+  )
+}
+
+export default PageContainer
