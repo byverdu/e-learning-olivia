8fc2c922b95e718d6e5f330576d5e29e9cf59b34
Albert Vallverdu <byverdu@gmail.com>
2020-09-07 10:43:03 +0100

    refactored how score is passed as prop and highlight first-letter for double digit

--- src/Components/Molecules/Scoreboard/Scoreboard.tsx
+++ src/Components/Molecules/Scoreboard/Scoreboard.tsx
@@ -1,51 +1,30 @@
 import React, {
   FunctionComponent,
-  useContext,
-  useCallback,
 } from 'react'
 import Icon from 'Components/Atoms/Icon'
-import { AppContext, Actions } from 'Store'
 import classNames from 'classnames'
 
+import { Score } from 'Store/store.types'
 import styles from './scoreboard.scss'
 
-const Scoreboard: FunctionComponent = () => {
-  const {
-    state: { score },
-    dispatch,
-  } = useContext(AppContext)
-  const icons = score.map(item => (
-    <Icon
-      key={item.id}
-      className={classNames(styles['score-star'], {
-        [styles.active]: item.active,
-      })}
-      name="star"
-    />
-  ))
-  const resetGame = useCallback(() => {
-    dispatch(Actions.gameReset())
-  }, [dispatch])
-  const increaseGame = useCallback(() => {
-    dispatch(Actions.gameIncrease())
-  }, [dispatch])
-  return (
-    <>
-      <button
-        onClick={resetGame}
-        type="button"
-      >
-        Reset
-      </button>
-      <button
-        onClick={increaseGame}
-        type="button"
-      >
-        UP UP
-      </button>
-      <div>{icons}</div>
-    </>
-  )
+interface Props {
+  score: Score[]
 }
+
+const Scoreboard: FunctionComponent<Props> = ({ score }: Props) => (
+  <>
+    <div>
+      {score.map(item => (
+        <Icon
+          key={item.id}
+          className={classNames(styles['score-star'], {
+            [styles.active]: item.active,
+          })}
+          name="star"
+        />
+      ))}
+    </div>
+  </>
+)
 
 export default Scoreboard
